4ed96ca328a015ce091e44fcd6211e2f13a31cfb
LorinWang <wangyimingfp@gmail.com>
2015-11-07 00:20:56 +0800

    Initial commit

---
+++ app/src/main/java/com/lorin/criminalintent/Crime.java
@@ -0,0 +1,51 @@
+package com.lorin.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Lorin on 2015/11/5.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate=new Date();
+    }
+}
---
+++ app/src/main/java/com/lorin/criminalintent/CrimeActivity.java
@@ -0,0 +1,31 @@
+package com.lorin.criminalintent;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        android.support.v4.app.FragmentManager fm=getSupportFragmentManager();
+        Fragment fragment =fm.findFragmentById(R.id.fragmentContainer);
+
+        if(fragment==null){
+            fragment =new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer,fragment).commit();
+
+        }
+    }
+}
